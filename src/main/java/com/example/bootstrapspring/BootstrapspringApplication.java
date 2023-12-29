e3cec197d3fde1a684dfcdcbbfeb696c2a854890
Adel <85290866+TotalGenius@users.noreply.github.com>
2023-12-29 07:18:54 +0300

    first commit

---
+++ src/main/java/com/example/bootstrapspring/BootstrapspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootstrapspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bootstrapspring/BootstrapspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootstrapspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrapspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
